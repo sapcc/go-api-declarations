85cc2b76a0dbc61d6222c5a80d371c73a2522711
Daniel Wagner <daniel.wagner03@sap.com>
2026-08-27 11:16:12 +0200

    add RFC3339 encoded time.Time

--- limes/time.go
+++ limes/time.go
@@ -31,3 +31,45 @@ func (t *UnixEncodedTime) UnmarshalJSON(buf []byte) error {
 	}
 	return err
 }
+
+// RFC3339EncodedTime is a time.Time that marshals into JSON as RFC3339 timestamp.
+type RFC3339EncodedTime struct {
+	time.Time
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (t RFC3339EncodedTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Format(time.RFC3339))
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *RFC3339EncodedTime) UnmarshalJSON(buf []byte) error {
+	var s string
+	err := json.Unmarshal(buf, &s)
+	if err != nil {
+		return err
+	}
+	t.Time, err = time.Parse(time.RFC3339, s)
+	return err
+}
+
+// RFC3339NanoEncodedTime is a time.Time that marshals into JSON as RFC3339Nano timestamp.
+type RFC3339NanoEncodedTime struct {
+	time.Time
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+func (t RFC3339NanoEncodedTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Format(time.RFC3339Nano))
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *RFC3339NanoEncodedTime) UnmarshalJSON(buf []byte) error {
+	var s string
+	err := json.Unmarshal(buf, &s)
+	if err != nil {
+		return err
+	}
+	t.Time, err = time.Parse(time.RFC3339Nano, s)
+	return err
+}
